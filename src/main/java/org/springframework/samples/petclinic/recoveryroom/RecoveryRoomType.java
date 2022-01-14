741eab6bb59d0b3d07ba98beedc5fc1bebc9d0fd
José Ignacio <garciaji700@gmail.com>
2022-01-14 17:24:30 +0100

    Entrega examen

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
@@ -1,11 +1,25 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoomType {
-    Integer id;
+@Entity
+@Table(name = "recoveryroomtypes")
+public class RecoveryRoomType extends BaseEntity {
+    //Id -> BaseEntity
+	
+	@NotNull
+	@Length(min = 5, max = 50)
+	@Column(name = "name", unique = true)
     String name;
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomTypeFormatter.java
@@ -3,20 +3,28 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RecoveryRoomTypeFormatter implements Formatter<RecoveryRoomType>{
+	
+	@Autowired
+	private RecoveryRoomService recoveryRoomService;
 
     @Override
     public String print(RecoveryRoomType object, Locale locale) {
-        return null;
+        return object.getName();
     }
 
     @Override
     public RecoveryRoomType parse(String text, Locale locale) throws ParseException {
-        return null;
+        RecoveryRoomType type = recoveryRoomService.getRecoveryRoomType(text);
+        if(type == null) {
+        	throw new ParseException("No es posible pasear el texto de entrada",0);
+        }
+    	return type;
     }
     
 }
